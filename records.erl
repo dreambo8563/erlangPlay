298b4da0038172ad2ba4a85743f38fd39fbca34f
dreambo8563 <dreambo8563@outlook.com>
2016-10-19 11:46:18 +0800

    record data extract

--- records.erl
+++ records.erl
@@ -1,7 +1,9 @@
 
 -module(records).
--export([todo/0]).
+-export([todo/0,printStatus/1]).
 -record(todo,{status=reminder,who=joe,text}).
 
 todo()->
     #todo{status=done}.
+printStatus(#todo{status=S,who=_,text=_})->
+    S.
