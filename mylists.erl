50964675895be5d81a6b259b125893071bd052eb
dreambo8563 <dreambo8563@outlook.com>
2016-10-19 17:18:18 +0800

    case construct

--- mylists.erl
+++ mylists.erl
@@ -1,11 +1,31 @@
 -module(mylists).
--export([sum/1,map/2]).
+-export([sum/1,map/2,filter/2,caseFilter/2]).
 
-sum([H|T]) ->
+sum([H|T])           ->
     H+sum(T);
-sum([])    ->0.
+sum([])              ->0.
 
 %% we don't care the F, so just _ as the placeholder
-map(_,[])->
+map(_,[])            ->
     [];
-map(F,[H|T]) ->[F(H)|map(F,T)].
+map(F,[H|T])         ->[F(H)|map(F,T)].
+
+filter(F,[H|T])      ->
+    filter1(F(H),H,F,T);
+filter(_,[])         ->[].
+
+filter1(true,H,F,T)  ->
+    [H|filter(F,T)];
+filter1(false,_,F,T) ->filter(F,T).
+
+caseFilter(F,[H|T])  ->
+    case F(H) of
+        true -> [H| caseFilter(F,T)];
+        false-> caseFilter(F,caseFilter(F,T))
+    end;
+caseFilter(_,[])->
+    [].
+
+
+
+
