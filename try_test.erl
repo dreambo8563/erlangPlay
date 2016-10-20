d82fbd13640b7bbd63e86c436839df32c6818832
dreambo8563 <dreambo8563@outlook.com>
2016-10-20 09:50:34 +0800

    stacktrace for exception in demo3

--- try_test.erl
+++ try_test.erl
@@ -1,5 +1,5 @@
 -module(try_test).
--export([generate_exception/1,demo1/0,demo2/0]).
+-export([generate_exception/1,demo1/0,demo2/0,demo3/0]).
 
 generate_exception(1)->
     a;
@@ -29,3 +29,9 @@ catcher(N)->
 
 demo2()->
     [{I,(catch generate_exception(I))} || I <- [1,2,3,4,5]].
+demo3()->
+    try generate_exception(5)
+    catch 
+        error:X ->
+            {X,erlang:get_stacktrace()}
+    end.
