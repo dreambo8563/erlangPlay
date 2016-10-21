b1f1451e1d1e6b20f679c5ee9f9b52fa674c69f1
dreambo8563 <dreambo8563@outlook.com>
2016-10-21 13:46:05 +0800

    max processs but not working --- too many process and terminal shell

---
+++ processes.erl
@@ -0,0 +1,49 @@
+%%%-------------------------------------------------------------------
+%%% @author  <Vincent@VINCENT>
+%%% @copyright (C) 2016, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21 Oct 2016 by  <Vincent@VINCENT>
+%%%-------------------------------------------------------------------
+-module(processes).
+
+%% API
+-export([max/1]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+max(N) ->
+    Max = erlang:system_info(process_limit),
+    io:format("Maximum allowed processes:~p~n",[Max]),
+    statistics(runtime),
+    statistics(wall_clock),
+    L = for(1, N, fun() -> spawn(fun() -> wait() end) end),
+    {_, Time1} = statistics(runtime),
+    {_, Time2} = statistics(wall_clock),
+    lists:foreach(fun(Pid) -> Pid ! die end, L),
+    U1 = Time1 * 1000 / N,
+    U2 = Time2 * 1000 / N,
+    io:format("Process spawn time=~p (~p) microseconds~n",
+              [U1, U2]).
+
+wait()->
+    receive
+        die ->
+            void
+    end.
+
+for(N,N,F)->
+    [F()];
+for(I,N,F) ->[F()|for(I-1,N,F)].
